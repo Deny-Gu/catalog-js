710d5aeb61c92616a07bd459c79412e5ce2e20b0
ttopolllo4ek <ttopolllo4ek@gmail.com>
2023-04-02 23:27:25 +0300

    catalog-js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 let catalogWrapper = document.querySelector('.catalog-wrapper');
 let filterList = document.querySelectorAll('.catalog-sidebar-filter li');
 let filterAll = document.getElementById('all');
+let btnAddItem = document.querySelector('.add-item');
+let popup = document.querySelector('.popup-add-item');
+let addBtn = document.getElementById('add-btn');
+let cancelBtn = document.getElementById('cancel-btn');
+let inputTitle = document.querySelector('form #title');
+let selectCategory = document.querySelector('form select');
+let inputPrice = document.querySelector('form #price');
+let inputFile = document.querySelector('form #file');
+
+
 
 function addItem (title, category, brend, price, img) {
     let itemDiv = document.createElement('div');
@@ -42,38 +52,89 @@ function addItem (title, category, brend, price, img) {
 
 let url = 'https://64284586161067a83b0ab0fb.mockapi.io/items';
 
-window.addEventListener('load', () => 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => 
-        data.forEach(item => 
-            addItem(item.title, item.category, item.brend, item.price, item.img)
-    ))
-);
-
-filterAll.addEventListener('click', async function getCatalogAll () { 
-    catalogWrapper.innerHTML = '',
-    
+async function getCatalogAll () { 
     await fetch(url)
     .then(response => response.json())
     .then(data => 
         data.forEach(item => 
             addItem(item.title, item.category, item.brend, item.price, item.img)
-    ))}
-);
-    
-filterList.forEach((itemList) => {
-    itemList.addEventListener('click', async function getCatalogOes () { 
-    catalogWrapper.innerHTML = '',
-    
+    ))};
+
+async function getCatalogCategory (itemList) {   
     await fetch(url)
     .then(response => response.json())
     .then(data => 
         data.forEach((item) => { 
-            if (item.category === itemList.id) {
+             if (item.category === itemList.id) {
                 addItem(item.title, item.category, item.brend, item.price, item.img)
             }
     }))
 }
+
+window.addEventListener('load', () => {
+    getCatalogAll();
+}
+);
+
+filterAll.addEventListener('click', () => {
+    catalogWrapper.innerHTML = '';
+    getCatalogAll();
+});
+
+    
+filterList.forEach((itemList) => {
+    itemList.addEventListener('click', () => {
+        catalogWrapper.innerHTML = '';
+        getCatalogCategory(itemList);
+    }
 )
 });
+
+btnAddItem.addEventListener('click', () => {
+    if (popup.classList.contains('hidden')) {
+        popup.classList.remove('hidden');
+    }
+})
+
+cancelBtn.addEventListener('click', () => {
+    if (!popup.classList.contains('hidden')) {
+        popup.classList.add('hidden');
+    }
+})
+
+addBtn.addEventListener('click', function postAddItem () { 
+    if (!popup.classList.contains('hidden')) {
+        popup.classList.add('hidden');
+    }
+    
+    let item = {
+        title: inputTitle.value,
+        category: selectCategory[0].value,
+        brend: 'gang',
+        price: inputPrice.value,
+        img: inputFile.value
+      };
+
+    async function postCatalogAll () { 
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(item)
+      })
+    .then(response => response.json())
+    .then(async function postI (data) { 
+        data[JSON.stringify(item)],
+        catalogWrapper.innerHTML = '',
+        getCatalogAll()
+    })
+    }
+
+    postCatalogAll();
+
+    inputTitle.value = '';
+    inputPrice.value = '';
+
+}
+);
